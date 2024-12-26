14b5d2ab6fff410869d014b1a3f8584326749584
FastSnake0 <fasn1900@yandex.ru>
2024-12-26 23:57:19 +0300

    train pipeline

---
+++ train_pipeline/utils/user_settings.py
@@ -0,0 +1,4 @@
+""" Конфигурационные переменные для пользователя """
+
+IMG_FILE: str = r"S:\xakaton\train_dataset_skoltech_train\train\images\9_1.tif"
+GJ_FILE: str = r"S:\xakaton\train_dataset_skoltech_train\train\osm\9.geojson"
